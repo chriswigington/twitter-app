6d2a7aa24dcd4eff46e3f1348db5d635b3491c1a
chriswigington <chriswigington@gmail.com>
2016-09-04 19:27:47 -0400

    Configures for use of MongoDB

--- config/database.php
+++ config/database.php
@@ -26,7 +26,7 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'mysql'),
+    'default' => env('DB_CONNECTION', 'mongodb'),
 
     /*
     |--------------------------------------------------------------------------
@@ -78,6 +78,18 @@ return [
             'schema' => 'public',
             'sslmode' => 'prefer',
         ],
+
+        'mongodb' => array(
+            'driver' => 'mongodb',
+            'host' => env('DB_HOST', 'localhost'),
+            'port' => env('DB_PORT', 27017),
+            'database' => env('DB_DATABASE', 'l5'),
+            'username' => env('DB_USERNAME', 'l5'),
+            'password' => env('DB_PASSWORD', '123456'),
+            'options' => array(
+                'db' => 'admin' // sets the authentication database required by mongo 3
+            )
+        ),
 
     ],
 
