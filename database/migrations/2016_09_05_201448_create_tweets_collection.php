262d2338a2ef3e3beeafbf7c10a11e2fe04a554a
chriswigington <chriswigington@gmail.com>
2016-09-05 16:50:05 -0400

    Add migration for Tweets collection

---
+++ database/migrations/2016_09_05_201448_create_tweets_collection.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Jenssegers\Mongodb\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTweetsCollection extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('tweets', function (Blueprint $collection) {
+            $collection->increments('id');
+            $collection->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('tweets');
+    }
+}
