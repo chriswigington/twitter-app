e0c974975983d99ce543b22271e0f05df77a60f8
chriswigington <chriswigington@gmail.com>
2016-09-05 17:02:35 -0400

    Fix capitalization on model "Tweet"

--- app/tweet.php
+++ app/tweet.php
@@ -3,8 +3,9 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Jenssegers\Mongodb\Eloquent\Model as Eloquent;
 
-class tweet extends Model
+class Tweet extends Eloquent
 {
     //
 }
